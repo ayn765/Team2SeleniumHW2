fa05abc5c637627903006027657cb11f6f1b3a81
= <=>
2021-03-30 22:53:34 -0400

    Initial commit

---
+++ src/Base.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class Base {
+    static String relativePath = "/resources/drivers/windows/chromedriver.exe";
+    static String chromeDriverPath = System.getProperty("user.dir") + relativePath;
+    public static String URL = "https://amazon.com";
+    static WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        driver = new ChromeDriver();
+        driver.get(URL);
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
